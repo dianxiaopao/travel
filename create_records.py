381618136550bd5b3be9f3a4a729f755c58a96cd
frowlwood <frowlwood@qq.com>
2017-03-04 10:04:55 +0800

    增加论坛分类的demo数据

--- create_records.py
+++ create_records.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.models import User
-user = User.objects.create_user(username='admin',password='admin',email='admin@gaomumu.com')
-user.save
+from travel.wsgi import *
+
+if __name__ == '__main__':
+    from django.apps import apps
+
+    i_apps = apps.get_app_configs()
+    for app in i_apps:
+        app_path = getattr(app, 'path')
+        app_name = getattr(app, 'label')
+        f_record_path = os.path.join(app_path, 'create_records.py')
+        if os.path.exists(f_record_path):
+            app_module = __import__(app_name + '.create_records')
+            cr = getattr(app_module, 'create_records')
+            cr.make_data()
